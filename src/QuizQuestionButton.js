155d20243e3dc27f205cc5f538c463b2f6c835e5
derek <derek.guyagoes@gmail.com>
2018-06-21 20:44:36 -0600

    end of step 5

--- src/QuizQuestionButton.js
+++ src/QuizQuestionButton.js
@@ -1,14 +1,17 @@
-import React, {Component} from 'react';
-import PropTypes from 'prop-types';
+import React, { Component } from "react";
 
 class QuizQuestionButton extends Component {
-    render() {
-        return (
-            <div>
-                <li><button>{this.props.button_text}</button></li>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <li>
+          <button>{this.props.button_text}</button>import Pimport PropTypes from 'prop-types';
+ropTypes from 'prop-types';
+
+        </li>
+      </div>
+    );
+  }
 }
 
 QuizQuestionButton.propTypes = {};
